704f250ea09918e085baadc2700442082ad3af3e
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-08-12 18:17:22 -0500

    New lecture

---
+++ project-01/project-01-app/src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Testimony() {
+  return (
+    <div className="containerTestimony">
+      <img className="imageTestimony" src="../images/testimony-juan.png" alt="Emma photography"/>
+      <div className="containerTextTestimony">
+        <p className="nameTestimony">Juan David in Colombia</p>
+        <p className="positionTestimony">Software Engineer on Rappi</p>
+        <p className="textTestimony">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+      </div>
+    </div>
+  );
+}
