6c10fe382f02dae316bad14f1ce322f40dbc5a58
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-08-16 21:18:00 -0500

    Finish lecture and 1st. project

--- project-01/project-01-app/src/App.js
+++ project-01/project-01-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
           image='ShawnWang'
           position='Software Engineer'
           company='Amazon'
-          testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+          testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life." 
         />
 
         <Testimonials 
@@ -22,7 +22,7 @@ function App() {
           image='SarahChima'
           position='Software Engineer'
           company='ChatDesk'
-          testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+          testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." 
         />
 
         <Testimonials 
@@ -31,7 +31,7 @@ function App() {
           image='EmmaBostian'
           position='Software Engineer'
           company='Spotify'
-          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." 
         />
       </div>
     </div>
--- project-01/project-01-app/src/components/Testimonials.jsx
+++ project-01/project-01-app/src/components/Testimonials.jsx
@@ -4,10 +4,14 @@ import '../stylesSheets/testimonials.css';
 function Testimony(props) {
   return (
     <div className="containerTestimony">
-      <img className="imageTestimony" src={require(`../images/testimony-${props.image}.png`)} alt="Juanda photography"/>
+      <img className="imageTestimony" src={require(`../images/testimony-${props.image}.png`)} alt="Juanda photography" />
       <div className="containerTextTestimony">
-        <p className="nameTestimony">{props.name} in {props.country}</p>
-        <p className="positionTestimony">{props.position} at {props.company}</p>
+        <p className="nameTestimony">
+          <b>{props.name}</b> in {props.country}
+          </p>
+        <p className="positionTestimony">
+          {props.position} at <b>{props.company}</b>
+          </p>
         <p className="textTestimony">"{props.testimony}"</p>
       </div>
     </div>
